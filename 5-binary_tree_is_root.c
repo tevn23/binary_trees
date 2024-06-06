f1ba6dd6ae5b25a6817d92ffb1382349d307a6b4
tevn23 <mabulatt23@gmail.com>
2024-06-06 22:52:13 +0200

    Added Task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,17 +3,17 @@
 /**
  * binary_tree_is_root - checks if a given node is a root
  * @node: Pointer to the node to check
- * 
+ *
  * Return: 1 if node is root, else 0 and 0 if node is NULL
 */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-    
-    if (node->parent == NULL)
-        return (1);
-    else
-        return (0);
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
