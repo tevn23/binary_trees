01b66e70c8e67d3e7c3aa3e7888abb9a4c71bb19
tevn23 <mabulatt23@gmail.com>
2024-06-06 21:32:35 +0200

    Task 2 betty fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * binary_tree_insert_right -  inserts a node as the right-child of other node
  * @parent: pointer to the node to insert the right-child in
  * @value: Value to be stored in the new node
  *
