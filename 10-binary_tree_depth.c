40067aed441e9bf0c2c69c7eb3447ae6bbb7ebad
tevn23 <mabulatt23@gmail.com>
2024-06-07 02:04:13 +0200

    Added Task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
-    
-    if (tree == NULL)
-        return (0);
-    
-    while (tree->parent)
-    {
-        depth++;
-        tree = tree->parent;
-    }
+	size_t depth = 0;
 
-    return (depth);
+	if (tree == NULL)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
 }
