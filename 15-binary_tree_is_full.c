64bb3071653e01e4a991321b1041d67810c4b6eb
tevn23 <mabulatt23@gmail.com>
2024-06-07 05:03:11 +0200

    Task 15 Hot fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,6 +10,9 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
+		return (0);
+	
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if ((tree->left == NULL && tree->right != NULL) ||
